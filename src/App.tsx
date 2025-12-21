0f3ba8a45a8b294f6b2c321d4a36f76fc43b713a
zhrb77 <zhrb77@gmail.com>
2025-12-20 20:18:34 -0500

    Assignment-7 Half Compelate

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+import Header from "./components/container/Header";
 import Hero from "./components/container/HeroSection";
+import About from "./components/container/About";
+import Portfolio from "./components/container/Portfolio";
+import Services from "./components/container/ServiceSection";
+import Contact from "./components/container/Contact";
+import Footer from "./components/container/Footer";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl">code here</h1>
-      {/* example */}
-      <Hero />
-    </>
+    <div className="min-h-screen w-full bg-white text-[#0A0D12] dark:bg-black dark:text-[#FDFDFD]">
+      <Header />
+      <main className="flex flex-col items-center p-0 w-full">
+        <Hero />
+        <About />
+        <Portfolio />
+        <Services />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
   );
 }
 
--- src/components/container/HeroSection/Hero.tsx
+++ src/components/container/HeroSection/Hero.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import Button from "../../ui/Button";
 
-const Hero = () => {
-  return <div>Hero</div>;
+const Hero: React.FC = () => {
+  return (
+    <section
+      id="hero"
+      className="relative flex flex-col lg:flex-row justify-center lg:justify-end items-center w-full gap-10"
+    >
+      <div className="lg:absolute flex flex-col items-start mt-[61px] top-32 left-4 lg:left-35 gap-2 w-[90%] max-w-[361px] lg:max-w-[653px] h-auto">
+        <h1 className="text-4xl sm:text-4xl lg:text-[56px] font-bold leading-11 lg:leading-17 tracking-[-2%] max-w-3xl">
+          Your Tech Partner for{" "}
+          <span className="text-[#FF6C37]">Smarter Growth</span>
+        </h1>
+        <p className="text-base sm:text-base lg:text-[20px] font-semibold leading-[30px] lg:leading-[34px]">
+          We deliver tailored IT solutions to help you scale with speed and
+          confidence.
+        </p>
+        <Button className="w-full lg:w-50 h-11 lg:h-12 mt-10">
+          Let's Talk
+        </Button>
+      </div>
+      <div className="flex w-full lg:w-[747px] h-auto lg:items-end lg:right-0 lg:z-[-1]">
+        <img
+          src="../assets/images/hero-light.png"
+          alt="Hero Image"
+          className="w-full h-auto block dark:hidden"
+        />
+        <img
+          src="../assets/images/hero-dark.png"
+          alt="Hero Image"
+          className="w-full h-auto hidden dark:block"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
